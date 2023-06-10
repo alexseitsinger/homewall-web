db1d2f14f9a0016f57ee5b45832d53e5f3a90571
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2023-06-10 10:26:34 -0400

    updates routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -8,10 +8,11 @@ import AuthenticationRequiredPage from "app/pages/authentication-required"
 import NotFoundPage from "app/pages/not-found"
 import WelcomePage from "app/pages/welcome"
 import HomePage from "app/pages/home"
-import NetworkCardsPage from "app/pages/network-cards"
+import CardsPage from "app/pages/cards"
 import SetupPage from "app/pages/setup"
 //import FirewallRulesPage from "app/pages/firewall-rules"
 //import FirewallStatusPage from "app/pages/firewall-status"
+import DashboardPage from "app/pages/dashboard"
 
 //<Route path="firewall"> <Route path="rules" element={( <RequireState name="isAuthenticated" value={true} redirectTo="/authentication-required"> <FirewallRulesPage /> </RequireState>)} />
 //<Route path="status" element={( <RequireState name="isAuthenticated" value={true} redirectTo="/authentication-required"> <FirewallStatusPage /> </RequireState>)} /> 
@@ -23,8 +24,9 @@ export default function AppRoutes(props) {
       <Route path="/" element={<Layout />}>
         <Route index element={(<RequireState name="isAuthenticated" value={true} redirectTo="/welcome"><HomePage /></RequireState>)} />
         <Route path="welcome" element={(<RequireState name="isAuthenticated" value={false} redirectTo="/"><WelcomePage /></RequireState>)} />
-        <Route path="setup" element={(<RequireState name="isAuthenticated" value={true} redirectTo="/"><SetupPage /></RequireState>)} />
-        <Route path="network-cards" element={(<RequireState name="isAuthenticated" value={true} redirectTo="/authentication-required"><NetworkCardsPage /></RequireState>)} />
+        <Route path="dashboard" element={(<RequireState name="isAuthenticated" value={true} redirectTo="/authentication-required"><DashboardPage /></RequireState>)} />
+        <Route path="setup" element={(<RequireState name="isAuthenticated" value={true} redirectTo="/authentication-required"><SetupPage /></RequireState>)} />
+        <Route path="cards" element={(<RequireState name="isAuthenticated" value={true} redirectTo="/authentication-required"><CardsPage /></RequireState>)} />
         <Route path="authentication-required" element={(<RequireState name="isAuthenticated" value={false} redirectTo={"/"}><AuthenticationRequiredPage /></RequireState>)} />
         <Route path="not-found" element={<NotFoundPage />} />
         <Route path="*" element={<NotFoundPage />} />
