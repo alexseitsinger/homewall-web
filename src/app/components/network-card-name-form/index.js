fb3a5a3fc15d1a6abd527ca6e6f516e64c47fa52
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2023-06-13 20:10:52 -0400

    added cnacel button; changed placeholder; added flex grid

--- src/app/components/network-card-name-form/index.js
+++ src/app/components/network-card-name-form/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 export default function NetworkCardNameForm(props) {
   const [ value, setValue ] = useState('')
@@ -17,23 +19,32 @@ export default function NetworkCardNameForm(props) {
     setValue(e.target.value)
   }
 
-  const placeholder = props.placeholder 
-    ? props.placeholder
-    : "New name"
+  const handleCancel = (e) => {
+    props.onCancel();
+  }
+  
+  const placeholder = typeof props.currentName !== 'undefined' 
+    ? props.currentName 
+    : "Name"
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3">
-        <Form.Control
-          type="text"
-          placeholder={placeholder}
-          value={value}
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Button type="submit" variant="primary">
-        Submit
-      </Button>
+    <Form onSubmit={handleSubmit} className="d-flex">
+        <div className="p-2 flex-grow-1">
+          <Form.Control
+            type="text"
+            placeholder={placeholder}
+            value={value}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="p-2">
+          <Button type="submit" variant="primary">
+            Save 
+          </Button>
+          <Button onClick={handleCancel} className="ms-2">
+            Cancel
+          </Button>
+        </div>
     </Form>
   )
 }
